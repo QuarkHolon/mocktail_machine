67de974c7a824ed10247f5d3c8fa94f57fc696f2
Hugues Le Bars <hugues@quarkholon.com>
2025-03-21 18:22:22 +0100

    bottles schema

--- mysql/create_bottles.sql
+++ mysql/create_bottles.sql
@@ -1,7 +1,8 @@
 CREATE TABLE bottles (
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL,
-  location VARCHAR(50) NOT NULL,
-  current_level FLOAT NOT NULL
+  location TINYINT UNSIGNED NOT NULL, -- Emplacement dans armoire réfrigérée
+  current_level FLOAT NOT NULL, -- Taux de remplissage de 0.00 à 100.00
+  is_active TINYINT(1) DEFAULT 1  -- 1 représente TRUE, 0 représente FALSE
 );
 
