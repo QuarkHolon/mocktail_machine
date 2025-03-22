16ed77f6dcf6c3f70101a658c33d5dfa854eeeb4
Hugues Le Bars <hugues@quarkholon.com>
2025-03-22 09:45:44 +0100

    mysql setup

--- mysql/create_mocktails_table.sql
+++ mysql/create_mocktails_table.sql
@@ -28,5 +28,66 @@ INSERT INTO mocktails (name, ingredients, recipe) VALUES
 ('Lemon Mint Cooler', 'Jus de citron vert (60%), Sirop de menthe (20%), Eau gazeuse (20%)', ''),
 ('Sparkling Mint', 'Jus de citron vert (40%), Sirop de menthe (20%), Eau gazeuse (40%)', '');
 
+-- Mojito Virgin
+UPDATE mocktails
+SET ingredient_quantities = '{"citron_vert": 50, "sirop_menthe": 20, "eau_gazeuse": 30}'
+WHERE name = 'Mojito Virgin';
+
+-- Sunset Passion
+UPDATE mocktails
+SET ingredient_quantities = '{"ananas": 60, "citron_vert": 20, "eau_gazeuse": 20}'
+WHERE name = 'Sunset Passion';
+
+-- Berry Sparkler
+UPDATE mocktails
+SET ingredient_quantities = '{"ananas": 70, "citron_vert": 10, "eau_gazeuse": 20}'
+WHERE name = 'Berry Sparkler';
+
+-- Citrus Fizz
+UPDATE mocktails
+SET ingredient_quantities = '{"citron_vert": 60, "sirop_menthe": 10, "eau_gazeuse": 30}'
+WHERE name = 'Citrus Fizz';
+
+-- Tropical Breeze
+UPDATE mocktails
+SET ingredient_quantities = '{"ananas": 50, "citron_vert": 20, "eau_gazeuse": 30}'
+WHERE name = 'Tropical Breeze';
+
+-- Spicy Maple Cooler
+UPDATE mocktails
+SET ingredient_quantities = '{"ananas": 60, "citron_vert": 20, "sirop_menthe": 10, "eau_gazeuse": 10}'
+WHERE name = 'Spicy Maple Cooler';
+
+-- Minty Pineapple
+UPDATE mocktails
+SET ingredient_quantities = '{"ananas": 60, "sirop_menthe": 10, "eau_gazeuse": 30}'
+WHERE name = 'Minty Pineapple';
+
+-- Citrus Mint Sparkle
+UPDATE mocktails
+SET ingredient_quantities = '{"citron_vert": 50, "sirop_menthe": 15, "eau_gazeuse": 35}'
+WHERE name = 'Citrus Mint Sparkle';
+
+-- Tropical Mint
+UPDATE mocktails
+SET ingredient_quantities = '{"ananas": 50, "citron_vert": 20, "sirop_menthe": 10, "eau_gazeuse": 20}'
+WHERE name = 'Tropical Mint';
+
+-- Pineapple Fizz
+UPDATE mocktails
+SET ingredient_quantities = '{"ananas": 70, "eau_gazeuse": 30}'
+WHERE name = 'Pineapple Fizz';
+
+-- Lemon Mint Cooler
+UPDATE mocktails
+SET ingredient_quantities = '{"citron_vert": 60, "sirop_menthe": 20, "eau_gazeuse": 20}'
+WHERE name = 'Lemon Mint Cooler';
+
+-- Sparkling Mint
+UPDATE mocktails
+SET ingredient_quantities = '{"citron_vert": 40, "sirop_menthe": 20, "eau_gazeuse": 40}'
+WHERE name = 'Sparkling Mint';
+
+
 
 
---
+++ mysql/insert_bottles.sql
@@ -0,0 +1,14 @@
+-- Initialisation des bouteilles avec des ingrédients liquides
+INSERT INTO bottles (name, location, current_level, is_active) VALUES
+('Jus de citron vert', 1, 75.0, 1),
+('Sirop de menthe', 2, 80.0, 1),
+('Jus d\'ananas', 3, 65.0, 1),
+('Eau gazeuse', 4, 90.0, 1),
+('Jus de pomme', 5, 50.0, 1),
+('Sirop de grenadine', 6, 40.0, 1),
+('Lait de coco', 7, 30.0, 1),
+('Jus de cranberry', 8, 20.0, 1);
+
+
+
+
